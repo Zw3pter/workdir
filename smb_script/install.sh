761eb7bacdd2cd4f843cbdbea9f425e9afc221a6
Zw3pter <148280896+Zw3pter@users.noreply.github.com>
2023-12-18 14:08:40 +0100

    Update install.sh

--- smb_script/install.sh
+++ smb_script/install.sh
@@ -15,15 +15,15 @@ sudo groupadd regnskab
 # Opret mapper og tildele rettigheder
 # faelles mappe oprettelse
 sudo mkdir -p /samba/faelles
-sudo chown root:faelles /samba/faelles
+sudo chown nobody:nobody /samba/faelles
 sudo chmod 0770 /samba/faelles
 # lager mappe oprettelse
 sudo mkdir -p /samba/lager
-sudo chown root:lager /samba/lager
+sudo chown nobody:nobody /samba/lager
 sudo chmod 0770 /samba/lager
 # regnskab mappe oprettelse
 sudo mkdir -p /samba/regnskab
-sudo chown root:regnskab /samba/regnskab
+sudo chown nobody:nobody /samba/regnskab
 sudo chmod 0770 /samba/regnskab
 
 # Konfigurer Samba skriver data til filen smb.conf
@@ -42,45 +42,48 @@ read only = nobody
 guest ok = yes
 
 [faelles]
-path = /samba/faelles
-valid users = @faelles
-writeable = yes
-browsable = yes
+	path = /samba/faelles
+	browsable = yes
+	writeable = yes
+	guest ok = yes
+	read only = no
 
 [lager]
-path = /samba/lager
-valid users = @lager
-writeable = yes
-browsable = yes
+	path = /samba/lager
+	browsable = yes
+	writeable = yes
+	guest ok = yes
+	read only = no
 
 [regnskab]
-path = /samba/regnskab
-valid users = @regnskab
-writeable = yes
-browsable = yes
+	path = /samba/regnskab
+	browsable = yes
+	writeable = yes
+	guest ok = yes
+	read only = no
 EOF'
 
 # Genstart Samba services
 sudo systemctl restart smb nmb
 
 # Opret bruger
-sudo useradd -M -d /samba/faelles -s /usr/sbin/nologin -G faelles userfaelles
-sudo useradd -M -d /samba/lager -s /usr/sbin/nologin -G lager userlager
-sudo useradd -M -d /samba/regnskab -s /usr/sbin/nologin -G regnskab userregnskab
+#sudo useradd -M -d /samba/faelles -s /usr/sbin/nologin -G faelles userfaelles
+#sudo useradd -M -d /samba/lager -s /usr/sbin/nologin -G lager userlager
+#sudo useradd -M -d /samba/regnskab -s /usr/sbin/nologin -G regnskab userregnskab
 
-echo Indstil brugerens Samba password interaktivt
-echo Du skal indtast password til 3 brugere
-echo forste bruger userfaelles 
-sudo smbpasswd -a userfaelles
-sudo smbpasswd -e userfaelles
+#echo Indstil brugerens Samba password interaktivt
+#echo Du skal indtast password til 3 brugere
+#echo forste bruger userfaelles 
+#sudo smbpasswd -a userfaelles
+#sudo smbpasswd -e userfaelles
 
-echo Indtast password for userlager
-sudo smbpasswd -a userlager
-sudo smbpasswd -e userlager
+#echo Indtast password for userlager
+#sudo smbpasswd -a userlager
+#sudo smbpasswd -e userlager
 
-echo Indtast password for userregnskab
-sudo smbpasswd -a userregnskab
-sudo smbpasswd -e userregnskab
+#echo Indtast password for userregnskab
+#sudo smbpasswd -a userregnskab
+#sudo smbpasswd -e userregnskab
 
 # Tilføj Samba til firewall og genindlæs
 sudo firewall-cmd --add-service=samba --permanent
