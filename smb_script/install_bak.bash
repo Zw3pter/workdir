f7fb2a589429ad3d9d5097f4d3a5db826e94bcf0
Zw3pter <148280896+Zw3pter@users.noreply.github.com>
2023-12-18 12:14:52 +0100

    Changes to files, edit to include kerberos setup

---
+++ smb_script/install_bak.bash
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# Installer Samba-pakker
+sudo dnf install samba samba-client samba-common -y
+
+# Start og aktiver Samba services
+sudo systemctl start smb nmb
+sudo systemctl enable smb nmb
+
+# Opret gruppe
+sudo groupadd faelles
+sudo groupadd lager
+sudo groupadd regnskab
+
+# Opret mapper og tildele rettigheder
+# faelles mappe oprettelse
+sudo mkdir -p /samba/faelles
+sudo chown root:faelles /samba/faelles
+sudo chmod 0770 /samba/faelles
+# lager mappe oprettelse
+sudo mkdir -p /samba/lager
+sudo chown root:lager /samba/lager
+sudo chmod 0770 /samba/lager
+# regnskab mappe oprettelse
+sudo mkdir -p /samba/regnskab
+sudo chown root:regnskab /samba/regnskab
+sudo chmod 0770 /samba/regnskab
+
+# Konfigurer Samba skriver data til filen smb.conf
+sudo bash -c 'cat << EOF > /etc/samba/smb.conf
+[Global]
+workgroup = WORKGROUP
+server string = This is a samba share on centos
+netbios name = FILSERVER
+wins support = yes
+security = user
+map to guest = bad user
+guest account = nobody
+
+browsable = yes
+read only = nobody
+guest ok = yes
+
+[faelles]
+path = /samba/faelles
+valid users = @faelles
+writeable = yes
+browsable = yes
+
+[lager]
+path = /samba/lager
+valid users = @lager
+writeable = yes
+browsable = yes
+
+[regnskab]
+path = /samba/regnskab
+valid users = @regnskab
+writeable = yes
+browsable = yes
+EOF'
+
+# Genstart Samba services
+sudo systemctl restart smb nmb
+
+# Opret bruger
+sudo useradd -M -d /samba/faelles -s /usr/sbin/nologin -G faelles userfaelles
+sudo useradd -M -d /samba/lager -s /usr/sbin/nologin -G lager userlager
+sudo useradd -M -d /samba/regnskab -s /usr/sbin/nologin -G regnskab userregnskab
+
+echo Indstil brugerens Samba password interaktivt
+echo Du skal indtast password til 3 brugere
+echo forste bruger userfaelles 
+sudo smbpasswd -a userfaelles
+sudo smbpasswd -e userfaelles
+
+echo Indtast password for userlager
+sudo smbpasswd -a userlager
+sudo smbpasswd -e userlager
+
+echo Indtast password for userregnskab
+sudo smbpasswd -a userregnskab
+sudo smbpasswd -e userregnskab
+
+# Tilføj Samba til firewall og genindlæs
+sudo firewall-cmd --add-service=samba --permanent
+sudo firewall-cmd --reload
+
+# Anvend SELinux kontekst
+sudo chcon -t samba_share_t /samba/faelles /samba/lager /samba/regnskab
---
+++ smb_script/script.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+# Installer Samba, Winbind og Kerberos-pakker
+sudo dnf install samba samba-client samba-common krb5-workstation krb5-libs samba-winbind samba-winbind-clients -y
+
+# Start og aktiver Samba og Winbind services
+sudo systemctl start smb nmb winbind
+sudo systemctl enable smb nmb winbind
+
+# Opret gruppe
+sudo groupadd faelles
+sudo groupadd lager
+sudo groupadd regnskab
+
+# Opret mapper og tildele rettigheder
+# faelles mappe oprettelse
+sudo mkdir -p /samba/faelles
+sudo chown root:faelles /samba/faelles
+sudo chmod 0770 /samba/faelles
+# lager mappe oprettelse
+sudo mkdir -p /samba/lager
+sudo chown root:lager /samba/lager
+sudo chmod 0770 /samba/lager
+# regnskab mappe oprettelse
+sudo mkdir -p /samba/regnskab
+sudo chown root:regnskab /samba/regnskab
+sudo chmod 0770 /samba/regnskab
+
+# Konfigurer Samba til AD integration i smb.conf
+sudo bash -c 'cat << EOF > /etc/samba/smb.conf
+[global]
+   workgroup = DCTRL-LTJ
+   security = ads
+   realm = DCTRL.LTJ.LOCAL
+   winbind use default domain = true
+   winbind enum users = yes
+   winbind enum groups = yes
+   idmap config * : backend = tdb
+   idmap config * : range = 1000-999999
+   idmap config DCTRL-LTJ : backend = rid
+   idmap config DCTRL-LTJ : range = 10000-999999
+   template shell = /bin/bash
+
+[faelles]
+   path = /samba/faelles
+   valid users = @faelles
+   writable = yes
+   browsable = yes
+
+[lager]
+   path = /samba/lager
+   valid users = @lager
+   writable = yes
+   browsable = yes
+
+[regnskab]
+   path = /samba/regnskab
+   valid users = @regnskab
+   writable = yes
+   browsable = yes
+EOF'
+
+# Genstart Samba og Winbind services
+sudo systemctl restart smb nmb winbind
+
+# Konfigurer Kerberos
+sudo bash -c 'cat << EOF > /etc/krb5.conf
+[logging]
+ default = FILE:/var/log/krb5libs.log
+
+[libdefaults]
+ default_realm = DCTRL.LTJ.LOCAL
+ dns_lookup_realm = false
+ dns_lookup_kdc = true
+ ticket_lifetime = 24h
+ renew_lifetime = 7d
+ forwardable = true
+
+[realms]
+ DCTRL.LTJ.LOCAL = {
+  kdc = 10.25.175.62
+  admin_server = 10.25.175.62
+ }
+
+[domain_realm]
+ .dctrl.ltj.local = DCTRL.LTJ.LOCAL
+ dctrl.ltj.local = DCTRL.LTJ.LOCAL
+EOF'
+
+# Tilføj Samba, Winbind og Kerberos til firewall og genindlæs
+sudo firewall-cmd --add-service=samba --permanent
+sudo firewall-cmd --add-service=kerberos --permanent
+sudo firewall-cmd --reload
+
+# Anvend SELinux kontekst
+sudo chcon -t samba_share_t /samba/faelles /samba/lager /samba/regnskab
